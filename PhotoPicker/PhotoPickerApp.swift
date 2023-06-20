e04475fe7e3ba3e4638315ac9c0b4db935d47522
jdeda <jldeda00@gmail.com>
2023-06-20 17:53:06 -0400

    Initial Commit

---
+++ PhotoPicker/PhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerApp.swift
+//  PhotoPicker
+//
+//  Created by Jesse Deda on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
