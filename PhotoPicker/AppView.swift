9407aa908fc707231ca490ae74e8195837824798
jdeda <jldeda00@gmail.com>
2023-06-20 19:29:54 -0400

    init

---
+++ PhotoPicker/AppView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct AppView: View {
+  var body: some View {
+    Text("AppView")
+  }
+}
+
+struct AppView_Previews: PreviewProvider {
+  static var previews: some View {
+    AppView()
+  }
+}
